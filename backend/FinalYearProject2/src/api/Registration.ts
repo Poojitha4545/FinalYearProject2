48210c71b9001a394c7d467a0b7484deb20255ef
Poojitha <poojithabandara123@gmail.com>
2026-05-16 22:45:26 +0530

    corrected the deployment issues

--- backend/FinalYearProject2/src/api/Registration.ts
+++ backend/FinalYearProject2/src/api/Registration.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register,login } from '../application/Registration';
+import { register, login } from '../application/Registration.js';
 
 const RegistrationRouter = express.Router();
 
