1ae3694995b8b23601b781ff2527b2771dc00bbe
Poojitha <poojithabandara123@gmail.com>
2026-05-17 16:29:17 +0530

    ........

--- frontend/Visit_Sri_Lanka/src/pages/home.page.jsx
+++ frontend/Visit_Sri_Lanka/src/pages/home.page.jsx
@@ -401,7 +401,7 @@ function InteractiveMap() {
       if (!window.google?.maps) {
         await new Promise((resolve, reject) => {
           const script = document.createElement("script");
-         script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&v=beta&libraries=marker`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&v=beta&libraries=marker`;
           script.async = true;
           script.onload = resolve;
           script.onerror = reject;
